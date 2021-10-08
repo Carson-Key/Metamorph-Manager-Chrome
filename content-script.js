fbbaf6d97c99ee6f5340d6707123ea406c2ae06e
Carson-Key <keyc@oregonstate.edu>
2021-10-08 16:29:24 -0700

    Switch to jquerry

--- content-script.js
+++ content-script.js
@@ -2,7 +2,13 @@ const url = document.URL;
 const regex = new RegExp("https://app.roll20.net/editor/character/*");
 
 if (regex.test(url)) {
-    console.log(document.getElementsByTagName("form"))
+    setTimeout(() => {
+        let tempObj = {}
+        const characterSheet = $("span:contains('Metamorph')")
+        Array.from(characterSheet).forEach(element => {
+            console.log(element.innerHTML)
+        });
+    }, 1000)
 } else {
     console.log("This is not a Roll20 character popout")
 }
