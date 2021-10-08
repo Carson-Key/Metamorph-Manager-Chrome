7f7c6e18079bbeeb1ac59e6d532c5aadb704e804
Carson-Key <keyc@oregonstate.edu>
2021-10-08 15:17:59 -0700

    Add better logging to main condition

--- content-script.js
+++ content-script.js
@@ -2,7 +2,7 @@ const url = document.URL;
 const regex = new RegExp("https://app.roll20.net/editor/character/*");
 
 if (regex.test(url)) {
-    console.log("Yes")
+    console.log(document.getElementsByTagName("form"))
 } else {
-    console.log("No")
+    console.log("This is not a Roll20 character popout")
 }
