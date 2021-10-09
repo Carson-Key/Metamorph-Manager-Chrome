601dcc377342df20854a437b58d2d9b99ccb6ef6
Carson-Key <keyc@oregonstate.edu>
2021-10-09 12:07:56 -0700

    Remove unnecessary line

--- src/helpers/roll20Popout.js
+++ src/helpers/roll20Popout.js
@@ -5,8 +5,7 @@ export const getMorphPowers = () => {
 }
 export const parsePowerDescription = (morph) => {
     const splitDesc = morph.innerHTML.split("\n")
-    const splitName = splitDesc[0].split(" ")
-    const metaName = splitName[2]
+    const metaName = splitDesc[0].split(" ")[2]
     const statBoosts = splitDesc.filter(line => line.includes('Increased'))
 
     return {
