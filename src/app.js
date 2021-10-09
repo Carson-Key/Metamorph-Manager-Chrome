6c71f7819681059a058cd1530dd21cd5ba6c93c8
Carson-Key <keyc@oregonstate.edu>
2021-10-08 22:22:33 -0700

    Extract roll20 popout specific code to content folder

--- src/app.js
+++ src/app.js
@@ -1,28 +1,8 @@
 import { decideContent } from './helpers/decideContent.js';
+import { roll20Popout } from './contents/roll20Popout.js';
 
 export function app() {
     const url = document.URL;
-
-    const roll20Popout = () => {
-        setTimeout(() => {
-            let statsIncrease = {}
-            const characterSheet = $("span:contains('Metamorph')")
-            Array.from(characterSheet).forEach(element => {
-                const splitDesc = element.innerHTML.split("\n")
-                const splitName = splitDesc[0].split(" ")
-                const metaName = splitName[2]
-                const statBoosts = splitDesc.filter(line => line.includes('Increased'))
-                let metaIncrease = {}
-                statBoosts.forEach((statLine) => {
-                    const statString = statLine.split(" ")
-                    metaIncrease[statString[2]] = parseInt(statString[3])
-                })
-                statsIncrease[metaName] = metaIncrease
-            });
-            console.log(statsIncrease)
-        }, 1000)
-    }
-    
     const contentList = [
         {
             regex: new RegExp("https://app.roll20.net/editor/character/*"),
---
+++ src/contents/roll20Popout.js
@@ -0,0 +1,19 @@
+export const roll20Popout = () => {
+    setTimeout(() => {
+        let statsIncrease = {}
+        const characterSheet = $("span:contains('Metamorph')")
+        Array.from(characterSheet).forEach(element => {
+            const splitDesc = element.innerHTML.split("\n")
+            const splitName = splitDesc[0].split(" ")
+            const metaName = splitName[2]
+            const statBoosts = splitDesc.filter(line => line.includes('Increased'))
+            let metaIncrease = {}
+            statBoosts.forEach((statLine) => {
+                const statString = statLine.split(" ")
+                metaIncrease[statString[2]] = parseInt(statString[3])
+            })
+            statsIncrease[metaName] = metaIncrease
+        });
+        console.log(statsIncrease)
+    }, 1000)
+}
