1565298c402af27e1f2c87b667858bbfa892742a
Carson-Key <keyc@oregonstate.edu>
2021-10-09 12:12:40 -0700

    Rename parsedPower to morphData

--- src/contents/roll20Popout.js
+++ src/contents/roll20Popout.js
@@ -5,13 +5,13 @@ export const roll20Popout = () => {
         let statsIncrease = {}
         const morphPowersArray = getMorphPowers()
         morphPowersArray.forEach(morph => {
-            const parsedPower = getMorphData(morph)
+            const morphData = getMorphData(morph)
             let metaIncrease = {}
-            parsedPower.stats.forEach((statLine) => {
+            morphData.stats.forEach((statLine) => {
                 const statString = statLine.split(" ")
                 metaIncrease[statString[2]] = parseInt(statString[3])
             })
-            statsIncrease[parsedPower.name] = metaIncrease
+            statsIncrease[morphData.name] = metaIncrease
         });
         console.log(statsIncrease)
     }, 3000)
