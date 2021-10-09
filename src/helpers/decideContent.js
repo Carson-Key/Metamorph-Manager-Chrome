06f0721bb605e48a7883a2ea8d355c565cab62cc
Carson-Key <keyc@oregonstate.edu>
2021-10-08 22:33:10 -0700

    Add break functionality to decideContent

--- src/helpers/decideContent.js
+++ src/helpers/decideContent.js
@@ -1,7 +1,14 @@
+let breakContentLoop = 0;
+
+const testRunContent = (url, content) => {
+    if (content.regex.test(url) && breakContentLoop !== 1) {
+        content.function();
+        breakContentLoop = 1;
+    }
+}
+
 export const decideContent = (url, contentList) => {
     contentList.forEach((content) => {
-        if (content.regex.test(url)) {
-            content.function();
-        }
+        testRunContent(url, content);
     });
 }
