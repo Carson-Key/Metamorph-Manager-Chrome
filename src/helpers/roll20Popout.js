35aab825d576ea327f57a761eee078a0b387d399
Carson-Key <keyc@oregonstate.edu>
2021-10-10 10:54:56 -0700

    Fix issue where nonpresent stats wouldn't be set to 0

--- src/helpers/roll20Popout.js
+++ src/helpers/roll20Popout.js
@@ -1,5 +1,35 @@
 import { convertHTMLCollectionToArray } from './basic.js'
 
+const STATLIST = {
+    strength: 0,
+    agility: 0,
+    fighting: 0,
+    awareness: 0,
+    stamina: 0,
+    dexterity: 0,
+    intellect: 0,
+    presence: 0,
+    dodge: 0,
+    parry: 0,
+    fortitude: 0,
+    toughness: 0,
+    will: 0,
+    initiave: 0,
+    acrobatics: 0,
+    athletics: 0,
+    deception: 0,
+    insight: 0,
+    intimidation: 0,
+    investigation: 0,
+    perception: 0,
+    persuasion: 0,
+    sleightOfHand: 0,
+    stealth: 0,
+    technology: 0,
+    treatment: 0,
+    vehicles: 0
+}
+
 const getMorphData = (morph) => {
     const splitDesc = morph.innerHTML.split("\n")
     const morphName = splitDesc[0].split(" ")[2]
@@ -16,7 +46,7 @@ const getStatIncreaseData = (statIncreaseLine, statIncreaseData) => {
     statIncreaseData[statIncreaseArray[2].toLowerCase()] = parseInt(statIncreaseArray[3])
 }
 const parseStatIncrease = (morphData) => {
-    let statIncreaseData = {}
+    let statIncreaseData = {...STATLIST}
 
     morphData.stats.forEach((statIncreaseLine) => {
         getStatIncreaseData(statIncreaseLine, statIncreaseData)
